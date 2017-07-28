e77ad892b4eed34dfaaf290aae22bd4c05214e74
Patrick Klingler <patrick@aphextwin.austin.rr.com>
2017-07-28 13:29:01 -0500

    Added some more fields in the dialog box, will add auto filling for those fields soon

--- PlaylistFiller.py
+++ PlaylistFiller.py
@@ -5,8 +5,9 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import spotipy.util as util
 import pprint
-from tkinter import Tk, Entry, Button, Label, StringVar, W
+from tkinter import Tk, Entry, Button, Label, StringVar, W, Text
 from time import sleep
+import os
 
 pp = pprint.PrettyPrinter(indent=1)
 playlist_uri = None
@@ -91,29 +92,49 @@ def parse_playlist_uri(raw_uri):
     raw_uri = raw_uri.split(':')
     return raw_uri[2], raw_uri[4]
 
-def get_playlist_uri_from_dialog():
+def get_playlist_info_from_dialog():
     master = Tk()
-    Label(master, text="Insert Playlist URI here").grid(row=0)
+    master.title("KVRX Playlist Filler")
+    Label(master, text="Playlist URI").grid(row=0)
+    Label(master, text="Playlist Title").grid(row=1)
+    Label(master, text="Playlist Date").grid(row=2)
+    Label(master, text="Show name").grid(row=3)
 
     playlist_uri = StringVar()
+    playlist_title = StringVar()
+    playlist_date = StringVar()
+    show_name = StringVar()
 
     playlist_uri_entry = Entry(master, textvariable=playlist_uri)
-    playlist_uri_entry.insert(10, 'spotify:user:1270874820:playlist:6pu0ez0zwoeJI5yBK30R3o')
+    playlist_uri_entry.insert(60, 'spotify:user:1270874820:playlist:6pu0ez0zwoeJI5yBK30R3o')
+
+    playlist_title_entry = Entry(master, textvariable=playlist_title)
+    playlist_title_entry.insert(60, 'Playlist Title')
+
+    playlist_date_entry = Entry(master, textvariable=playlist_date)
+    playlist_date_entry.insert(10, 'MM/DD/YYYY')
+
+    show_name_entry = Entry(master, textvariable=show_name)
+    show_name_entry.insert(60, 'The Lab')
 
     playlist_uri_entry.grid(row=0, column=1)
+    playlist_title_entry.grid(row=1, column=1)
+    playlist_date_entry.grid(row=2, column=1)
+    show_name_entry.grid(row=3, column=1)
 
-    Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
-    Button(master, text='Submit', command=master.quit).grid(row=3, column=1, sticky=W, pady=4)
+    Button(master, text='Quit', command=master.quit).grid(row=4, column=0, sticky=W, pady=4)
+    Button(master, text='Submit', command=master.quit).grid(row=4, column=1, sticky=W, pady=4)
     master.mainloop()
     master.destroy()
-    return playlist_uri.get()
+    return playlist_uri.get(), playlist_title.get(), playlist_date.get(), show_name.get()
 
 
 if __name__ == "__main__":
+
     # get playlist uri from user
-    playlist_uri = get_playlist_uri_from_dialog()
+    playlist_uri, playlist_title, playlist_date, show_name = get_playlist_info_from_dialog()
     username, playlist_id = parse_playlist_uri(playlist_uri)
-    print(username, playlist_id)
+    print(username, playlist_id, playlist_date, playlist_title, show_name)
 
     token = util.prompt_for_user_token(username, scope)
 
@@ -121,6 +142,10 @@ if __name__ == "__main__":
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
     playlist = create_playlist(sp,username, playlist_id)
+
+    playlist['playlist_title'] = playlist_title
+    playlist['playlist_date'] = playlist_date
+    playlist['show_name'] = show_name
 
     try:
         driver = webdriver.Chrome()
